2f9a839bdd35c0a940a9354ce44da38bd395c5b8
nnthach173 <83439525+nnthach173@users.noreply.github.com>
2021-05-08 21:10:49 +0700

    commit

---
+++ JavaSwing16_ProgessBar/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Progess bar = visual aid to let the user know that an operation is processing
+	
+		ProgressBarDemo demo = new ProgressBarDemo();
+	}
+
+}
